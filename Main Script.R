7ce606a7c7ba87f371b2780339d4da07a9c6c86c
Benny24aa <harleyb101010@gmail.com>
2025-03-16 13:25:16 +0000

    Main Script added

---
+++ Main Script.R
@@ -0,0 +1,13 @@
+source("C:/Users/benny/Documents/My Resps/NHS-Geography-Code-Lookup-File/Geo File.R")
+Cancer_Data_Mortality_HB <- get_resource(res_id = "57f0983f-864e-4dbd-b3dc-ea8f16de83a4")
+Cancer_Data_Mortality_CA <- get_resource(res_id = "eebc8f38-7297-4bdc-a417-69ce3e2e6d44")
+
+
+Cancer_Data_Mortality_CA <- Cancer_Data_Mortality_CA %>% 
+  rename(GeoCode = CA)
+
+Cancer_Data_Mortality_HB <- full_join(Cancer_Data_Mortality_HB, HB_Lookup, by = "HB") %>% 
+  mutate(DataTye = "Mortality")
+
+Cancer_Data_Mortality_CA <- full_join(Cancer_Data_Mortality_CA, Council_Lookup, by = "GeoCode") %>% 
+  mutate(DataTye = "Mortality")
